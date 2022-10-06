062a45b8d60754135afc937446cb4eaf52baca30
Ezequiel <oliverm316@gmail.com>
2022-10-06 16:28:11 +0200

    mongodb connection

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,8 +1,25 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
 import { connect } from 'mongoose'
 
-async function dbConnect(): Promise<void> {
-  const DB_URI = <string>process.env.DB_URI
+dotenv.config({ path: './config.env' })
+
+const DB_URI = <string>(
+  process.env.DATABASE?.replace(
+    '<PASSWORD>',
+    <string>process.env.DATABASE_PASSWORD
+  )
+)
+
+const dbConnect = async (): Promise<void> => {
+  try {
+    const connection = await connect(DB_URI)
+    console.log(
+      `MongoDB connected: ${connection.connection.host} ${connection.connection.port}`
+    )
+  } catch (err: any) {
+    console.error(`DB connection error: ${err.message}`)
+    process.exit(1)
+  }
   await connect(DB_URI)
 }
 
