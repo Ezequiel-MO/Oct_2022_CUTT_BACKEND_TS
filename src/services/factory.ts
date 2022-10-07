2ae75adbc43f6e330accbce24e28c423e7010a4e
Ezequiel <oliverm316@gmail.com>
2022-10-07 17:38:50 +0200

    catchAsync wrapper implemented to factory functions

--- src/services/factory.ts
+++ src/services/factory.ts
@@ -1,100 +1,73 @@
 import { NextFunction, Request, Response } from 'express'
 import { Model } from 'mongoose'
 import AppError from '../utils/appError'
-import { handleHttp } from '../utils/error.handle'
+import catchAsync from '../utils/catchAsync'
 
-const getOne = (Model: Model<any>) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
+const getOne = (Model: Model<any>) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params
-    try {
-      const doc = await Model.findById(id)
-      if (!doc) {
-        return next(new AppError(`No document found with that ID`, 404))
+    const doc = await Model.findById(id)
+    if (!doc) next(new AppError(`No document found with that ID`, 404))
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
       }
-      res.status(200).json({
-        status: 'success',
-        data: {
-          data: doc
-        }
-      })
-    } catch (e) {
-      handleHttp(res, 'ERROR_GET_ITEM', e)
-    }
-  }
-}
+    })
+  })
 
-const getAll = (Model: Model<any>) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const response = await Model.find({})
-      res.status(200).json({
-        status: 'success',
-        results: response.length,
-        data: {
-          data: response
-        }
-      })
-    } catch (e) {
-      handleHttp(res, 'ERROR_GET_ITEMS', e)
-    }
-  }
-}
+const getAll = (Model: Model<any>) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const response = await Model.find({})
+    res.status(200).json({
+      status: 'success',
+      results: response.length,
+      data: {
+        data: response
+      }
+    })
+  })
 
-const updateOne = (Model: Model<any>) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
+const updateOne = (Model: Model<any>) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params
-    try {
-      const doc = await Model.findByIdAndUpdate(id, req.body, {
-        new: true,
-        runValidators: true
-      })
-      if (!doc) {
-        return next(new AppError(`No document found with that ID`, 404))
+    const doc = await Model.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true
+    })
+    if (!doc) {
+      return next(new AppError(`No document found with that ID`, 404))
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
       }
-      res.status(200).json({
-        status: 'success',
-        data: {
-          data: doc
-        }
-      })
-    } catch (e) {
-      handleHttp(res, 'ERROR_UPDATE_ITEM', e)
-    }
-  }
-}
+    })
+  })
 
-const createOne = (Model: Model<any>) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const doc = await Model.create(req.body)
-      res.status(201).json({
-        status: 'success',
-        data: {
-          data: doc
-        }
-      })
-    } catch (e) {
-      handleHttp(res, 'ERROR_CREATE_ITEM', e)
-    }
-  }
-}
+const createOne = (Model: Model<any>) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const doc = await Model.create(req.body)
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    })
+  })
 
-const deleteOne = (Model: Model<any>) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
+const deleteOne = (Model: Model<any>) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params
-    try {
-      const doc = await Model.findByIdAndDelete(id)
-      if (!doc) {
-        return next(new AppError(`No document found with that ID`, 404))
-      }
-      res.status(204).json({
-        status: 'success',
-        data: null
-      })
-    } catch (e) {
-      handleHttp(res, 'ERROR_DELETE_ITEM', e)
+    const doc = await Model.findByIdAndDelete(id)
+    if (!doc) {
+      return next(new AppError(`No document found with that ID`, 404))
     }
-  }
-}
+    res.status(204).json({
+      status: 'success',
+      data: null
+    })
+  })
 
 export { getAll, getOne, updateOne, createOne, deleteOne }
